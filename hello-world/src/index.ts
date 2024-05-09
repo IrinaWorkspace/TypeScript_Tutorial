0624820f6865c391b57b31f14b0d703e6dc32741
Irina Spasova <irina.spasova.81@gmail.com>
2024-05-09 17:19:27 +0300

    Tuples

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,7 +1,35 @@
-let age: number = 20;
+// let sales: number = 123_456_789;
 
-if (age < 50) {
-  age += 10;
-}
+// let course: string = "TypeScript";
 
-console.log(age);
+// let is_published: boolean = true;
+
+// let sales: 123_456_789;
+
+// let course: "TypeScript";
+
+// let is_published: boolean = true;
+
+// let other;
+// other = 1;
+// other = "a";
+
+// function render(document) {
+//   console.log(document);
+// }
+
+// solving the problem:
+
+// function render(document:any) {
+//     console.log(document);
+//   }
+
+// or go to tsconfig and :
+//   /* Type Checking */
+// "strict": true /*
+// "noImplicitAny": false,
+
+//let numbers = [1,2,'3']; // JS
+
+//let numbers: number[] = [1,2,'3']; //TS
+//let numbers: number[] = [1, 2, 3]; //TS  === let numbers:  [1, 2, 3];
