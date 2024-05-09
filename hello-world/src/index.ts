11dc27df3e226bdf60ef7c5105cd33d4c5fe777f
Irina Spasova <irina.spasova.81@gmail.com>
2024-05-09 23:32:16 +0300

    objects

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -58,3 +58,39 @@
 // //tsc
 // // dist/index.js
 // !important- if we define the enum as constant ( const enum)- the code into the JS file will be optimized.
+
+//explanation of noneUnusedParams
+
+// Functions: https://youtu.be/d56mG7DezGs?t=2276
+// "noUnusedParameters": true, explanation
+// function calculateTax(income: number): number {
+//   // test one, this explain, that income is declared, but never used:
+//   // return 0;
+
+//   //test two, this explain, that  : number is "Function lacks ending return statement and return type does not include 'undefined'."
+// //   if (income < 500) {
+// //     return income * 1.2;
+// //   }
+// }
+
+// function calculateTax(income: number, ) {
+//   // let x; // this (unused variables) is prevent from // "noUnusedLocals": true,
+
+//   // test three- "noImplicitReturns": true,   explanation:
+//   if (income < 500) return income * 1.2;
+//   //there is a bug- this will return undefined, if income is > then 500
+//   // Not all code paths return a value.ts(7030) warning for calculateTax:
+//   return income * 1.3;
+
+//   //But it is very important ot always return value
+//  }
+
+function calculateTax(income: number, taxYear = 2022): number {
+  // ? make the argument optional
+  // 2 option to avoid:
+  //01. taxYear? + (taxYear || 2022)
+  //02. taxYear=2022 + (taxYear < 2022)
+  if ((taxYear || 2022) < 2022) return income * 1.2;
+  return income * 1.3;
+}
+calculateTax(10_0000, 2022);
